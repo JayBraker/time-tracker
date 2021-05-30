c615aa9771e46bd72bb3b2b224ccd38644a080c4
Josha Bartsch <jaybraker1@gmail.com>
2021-05-30 13:44:34 +0200

    push to git

---
+++ app.py
@@ -0,0 +1,411 @@
+from os import write
+import sys, configparser, sqlite3
+from PyQt5.QtCore import Qt, QTimer, QFile, QTextStream
+from PyQt5.QtWidgets import (
+    QApplication, QHBoxLayout, QLabel, QMainWindow, QMessageBox, QInputDialog,
+    QWidget, QVBoxLayout, QGridLayout, QPushButton, QLineEdit,
+    QSizePolicy, QDialog, QDialogButtonBox
+)
+from main_view_ui import Ui_MainWindow
+from BreezeStyleSheets import breeze_resources
+from flowlayout import FlowLayout
+from datetime import date, datetime, timedelta
+
+
+class Window(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        file = QFile("BreezeStyleSheets/dark.qss")
+        file.open(QFile.ReadOnly | QFile.Text)
+        stream = QTextStream(file)
+        self.setStyleSheet(stream.readAll())
+        self.project_format = "Flex_Grid"
+        self.project_dict = {}
+
+        self.config = configparser.ConfigParser()
+        self.config.read_file(open('time-tracker.config'))
+
+        if self.config.has_section('state'):
+            self.database_file = self.config['state'].get('file')
+            self.populate_from_db()
+
+            if 'auto_save' in self.config['state']:
+                self.auto_save = QTimer()
+                self.auto_save.timeout.connect(self.write_state)
+                self.auto_save.start(int(self.config['state'].get('auto_save')))
+
+        self.connectSignalsSlots()
+
+    def populate_from_db(self):
+        if self.database_file:
+            print("Attempting to load State from {}".format(self.database_file))
+            projects_scheme = self.config['SQL'].get('projects_scheme').split(', ')
+            tasks_scheme = self.config['SQL'].get('tasks_scheme').split(', ')
+            with sqlite3.connect(self.database_file) as db:
+                conn = db.cursor()
+                try:
+                    print("Loading Projects...")
+                    conn.execute("SELECT {keys} FROM projects;".format(
+                                    keys=", ".join(projects_scheme)))
+                    projects_tuple = conn.fetchall()
+                    for project in projects_tuple:
+                        self.project_dict[project[1]] = {
+                                                        "id": project[0],
+                                                        "name": project[1],
+                                                        "started_at": project[2],
+                                                        "ended_at": project[3],
+                                                        "tasks": {},
+                                                        "tab": None,
+                                                        }
+                    print("Projects loaded.")
+                except Exception as e:
+                    print(e)
+                try:
+                    print("Loading tasks...")
+                    for project in self.project_dict:
+                        conn.execute("SELECT {keys} FROM tasks WHERE project_id = {pid};".format(keys=", ".join(tasks_scheme), pid=self.project_dict[project]['id']))
+                        tasks_tuple = conn.fetchall()
+                        print(tasks_tuple)
+                        for task in tasks_tuple:
+                            self.project_dict[project]["tasks"][task[2]] = {
+                                                "id": task[0],
+                                                "project_id": task[1],
+                                                "name": task[2],
+                                                "started_at": task[3],
+                                                "ended_at": task[4],
+                                                "time_slots": [],
+                                                "task_obj": None,
+                                                "count": task[5],
+                                                }
+                            print("Loaded Task {}".format(task[2]))
+                            sql = "SELECT SUM(count) FROM timestamps WHERE task_id = {};".format(task[0])
+                            conn.execute(sql)
+                            cnt = conn.fetchone()[0]
+                            self.project_dict[project]["tasks"][task[2]]["count"] = int(cnt)
+                    print("All tasks loaded.")
+
+                except Exception as e:
+                    print(e)
+            self.draw_state()
+
+    def draw_state(self):
+        for project in self.project_dict:
+            print("Drawing {}".format(project))
+            self.new_project(project)
+
+            for task in self.project_dict[project]["tasks"]:
+                print("Drawing {}".format(task))
+                self.new_task(project, task)
+
+    def write_state(self):
+        projects_scheme = self.config['SQL'].get('projects_scheme').split(', ')
+        tasks_scheme = self.config['SQL'].get('tasks_scheme').split(', ')
+        to_db = {"projects": [], "tasks": [], "timestamps": []}
+
+        for project in self.project_dict:
+            temp_proj = {}
+            for key in projects_scheme:
+                if key in self.project_dict[project]:
+                    temp_proj[key] = self.project_dict[project][key]
+                else:
+                    temp_proj[key] = ""
+            to_db["projects"].append(temp_proj)
+
+            for task in self.project_dict[project]["tasks"]:
+                for timestamp in self.project_dict[project]["tasks"][task]["time_slots"]:
+                    to_db["timestamps"].append(timestamp)
+
+                temp_task = {}
+                for key in tasks_scheme:
+                    if key in self.project_dict[project]["tasks"][task]:
+                        temp_task[key] = self.project_dict[project]["tasks"][task][key]
+                    else:
+                        temp_task[key] = ""
+                to_db["tasks"].append(temp_task)
+        
+        with sqlite3.connect(self.database_file) as db:
+            cursor = db.cursor()
+            for item in to_db["projects"]:
+                sql_insert = "REPLACE INTO {} ({}) VALUES('{}');".format("projects",
+                    ", ".join([key for key in item]),
+                    "', '".join([str(item[key]) for key in item]))
+                cursor.execute(sql_insert)
+            for item in to_db["tasks"]:
+                sql_insert = "REPLACE INTO {} ({}) VALUES('{}');".format("tasks",
+                    ", ".join([key for key in item]),
+                    "', '".join([str(item[key]) for key in item]))
+                cursor.execute(sql_insert)
+            for item in to_db["timestamps"]:
+                sql_insert = "INSERT INTO {} ({}) VALUES('{}');".format("timestamps",
+                    ", ".join([key for key in item]),
+                    "', '".join([str(item[key]) for key in item]))
+                cursor.execute(sql_insert)
+
+    def connectSignalsSlots(self):
+        self.action_about.triggered.connect(self.about)
+        self.action_new_projekt.triggered.connect(self.new_project)
+
+    def closeEvent(self, event):
+        print("Saving state...")
+        self.write_state()
+        print("Finished!")
+        self.close
+
+    def register_db_id(self, type: str, object_dict):
+        if type == "project":
+            type = "projects"
+            scheme = self.config['SQL'].get('projects_scheme').split(', ')
+        elif type == "task":
+            type = "tasks"
+            scheme = self.config['SQL'].get('tasks_scheme').split(', ')
+        with sqlite3.connect(self.database_file) as db:
+            cursor = db.cursor()
+            sql_insert = "INSERT INTO {} ({}) VALUES('{}');".format(type,
+                ", ".join([item for item in scheme if item in object_dict and object_dict[item]]),
+                "', '".join([str(object_dict[item]) for item in object_dict if item in scheme and object_dict[item]]))
+            print(sql_insert)
+            cursor.execute(sql_insert)
+            cursor.execute("SELECT id FROM {} WHERE name = '{}';".format(type, object_dict['name']))
+            id = cursor.fetchone()[0]
+
+            return id
+            
+    def showTime(self, task):
+        # checking if flag is true
+        if task.flag:
+            task.count += 1
+        text = str(timedelta(seconds=task.count))
+        task.zLabel.setText(text)
+
+    def start_stopwatch(self, task, project_name):
+        task.flag = True
+        task.active_timer["started_at"] = datetime.now()
+
+    def stop_stopwatch(self, task, project_name):
+        task.flag = False
+        timer = task.active_timer
+        timer["ended_at"] = datetime.now()
+        delta = timer["ended_at"] - timer["started_at"]
+        timer["count"] = delta.total_seconds()
+
+        task_dict = self.project_dict[project_name]["tasks"][task.task_name]
+        task_dict["time_slots"].append(task.active_timer.copy())
+        task.active_timer = {"started_at": None,
+                             "ended_at": None,
+                             "task_id": task_dict["id"],
+                             "count": 0
+                            }
+
+    def delete_task(self, task):
+        if task.flag:
+            self.stop_stopwatch(task, task.project_name)
+        dlg = DeleteDialog(task.task_name)
+        if dlg.exec():
+            print(task.parent().vLayout.gLayout.removeWidget(task))
+            task_id = self.project_dict[task.project_name]["tasks"][task.task_name]["id"]
+            self.project_dict[task.project_name]["tasks"].pop(task.task_name)
+            with sqlite3.connect(self.database_file) as db:
+                cursor = db.cursor()
+                cursor.execute("DELETE FROM tasks WHERE id = {}".format(task_id))
+            print(task_id)
+
+            task.deleteLater()
+
+    def summarize_time(self, tab):
+        project = self.project_dict[tab.project_name]
+        total_count = 0
+        for task in project["tasks"]:
+            if "task_obj" in project["tasks"][task] and project["tasks"][task]["task_obj"]:
+                total_count += project["tasks"][task]["task_obj"].count
+        text = str(timedelta(seconds=total_count))
+        tab.zLabel.setText(text)
+
+    def new_project(self, project_name=None):
+        if not project_name:
+            project_name, ok = QInputDialog.getText(self,
+                                                "Name des Projekts",
+                                                "Projektname",
+                                                QLineEdit.Normal,
+                                                "Neues Projekt")
+            if not ok:
+                return
+            if project_name in self.project_dict:
+                self.statusBar().showMessage(
+                    'Es existiert bereits ein Projekt unter diesem Namen!')
+                return
+            project_dict = {
+                "id": None,
+                "name": project_name,
+                "started_at": datetime.now(),
+                "ended_at": None,
+                "tab": None,
+                "tasks": {}
+                }
+            project_dict["id"] = self.register_db_id("project", 
+                                                     project_dict.copy())
+            self.project_dict[project_name] = project_dict
+        else:
+            ok = True
+
+        if ok:
+            tab = QWidget()
+
+            tab_timer = QTimer(tab)
+
+            tab.project_name = project_name
+            self.ProjektVerzeichnis.addTab(tab, project_name)
+            vLayout = QVBoxLayout(tab)
+            vLayout.setAlignment(Qt.AlignTop)
+            
+            tab.zLabel = QLabel(str(timedelta(seconds=0)))
+
+            vLayout.addWidget(tab.zLabel, 0, Qt.AlignTop)
+
+            add_task_button = QPushButton("Neue Aufgabe hinzufügen")
+            add_task_button.setShortcut("Ctrl+T")
+            add_task_button.clicked.connect(lambda: self.new_task(project_name))            
+
+            vLayout.addWidget(add_task_button, 0, Qt.AlignTop)
+            if self.project_format == "Flex_Grid":
+                gLayout = FlowLayout()
+            elif self.project_format == "Fix_Grid":
+                gLayout = QGridLayout()
+            
+            gLayout.setAlignment(Qt.AlignTop)
+            vLayout.addLayout(gLayout)
+
+            tab.setLayout(vLayout)
+            vLayout.gLayout = gLayout
+            tab.vLayout = vLayout
+
+            tab_timer.timeout.connect(lambda: self.summarize_time(tab))
+            tab_timer.start(1000)
+
+            self.project_dict[project_name]["tab"] = tab
+            
+    def new_task(self, project_name, task_name=None):
+        if not task_name:
+            task_name, ok = QInputDialog.getText(self,
+                                                    "Name der Aufgabe",
+                                                    "Tätigkeit",
+                                                    QLineEdit.Normal,
+                                                    "Neue Aufgabe")
+            if not ok:
+                return
+            if task_name in self.project_dict[project_name]["tasks"]:
+                self.statusBar().showMessage(
+                    'Es existiert bereits eine Aufgabe unter diesem Namen!')
+                return
+            task_dict = {
+                "id": None,
+                "project_id": self.project_dict[project_name]["id"],
+                "name": task_name,
+                "started_at": datetime.now(),
+                "ended_at": None,
+                "time_slots": [],
+                "task_obj": None,
+                "count": 0,
+                }
+            task_dict["id"] = self.register_db_id("task", task_dict.copy())
+            self.project_dict[project_name]["tasks"][task_name] = task_dict
+        else:
+            ok = True
+
+        if ok:
+            tab = self.project_dict[project_name]["tab"]
+
+            task = QWidget()
+            task.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+            task.task_name = task_name
+            task.project_name = project_name
+            task_dict = self.project_dict[project_name]["tasks"][task.task_name]
+            task.active_timer = {"started_at": None,
+                                 "ended_at": None,
+                                 "task_id": task_dict["id"],
+                                 "count": 0
+                                 }
+            self.project_dict[project_name]["tasks"][task_name]["task_obj"] = task
+
+            task.flag = False
+            task.count = task_dict["count"]
+
+            task.timer = QTimer(task)
+            verticalLayout = QVBoxLayout(task)
+            horizontalLayout = QHBoxLayout()
+            delete_task_button = QPushButton("Aufgabe löschen")
+            delete_task_button.setStyleSheet("background-color : red;")
+            push_button_start = QPushButton()
+            push_button_stop = QPushButton()
+            nLabel = QLabel(task_name)
+            zLabel = QLabel(str(timedelta(seconds=task.count)))
+
+            task.zLabel = zLabel
+            
+            verticalLayout.addWidget(nLabel, 0, Qt.AlignTop)
+            verticalLayout.addWidget(zLabel, 0, Qt.AlignTop)
+            
+            push_button_start.setObjectName(u"start_timer")
+            push_button_stop.setObjectName(u"stop_timer")
+            push_button_start.setText("Start Timer")
+            push_button_stop.setText("Stop Timer")
+
+            delete_task_button.clicked.connect(lambda: self.delete_task(task))
+            push_button_start.clicked.connect(
+                            lambda: self.start_stopwatch(task, project_name))
+            push_button_stop.clicked.connect(
+                            lambda: self.stop_stopwatch(task, project_name))
+
+            horizontalLayout.addWidget(push_button_start, 0, Qt.AlignBottom)
+            horizontalLayout.addWidget(push_button_stop, 0, Qt.AlignBottom)
+
+            verticalLayout.addLayout(horizontalLayout)
+            verticalLayout.addWidget(delete_task_button, 0, Qt.AlignBottom)
+
+            task.layout = verticalLayout
+            task.setStyleSheet("background: rgba(239, 240, 241, 60);")
+
+            task.timer.timeout.connect(lambda: self.showTime(task))
+            task.timer.start(1000)
+
+            tab.vLayout.gLayout.addWidget(task)
+            #tab.vLayout.addWidget(task, 0, Qt.AlignTop)
+
+    def about(self):
+        QMessageBox.about(
+            self,
+            "About Time-Tracker",
+            "<p>Anwendung zur Aufzeichnung der Arbeitszeit nach Projekten</p>"
+            "<p>Powered by:</p>"
+            "<p>- PyQt</p>"
+            "<p>- Qt Designer</p>"
+            "<p>- Python</p>",
+        )
+
+
+class DeleteDialog(QDialog):
+    def __init__(self, item_name):
+        super().__init__()
+
+        self.setWindowTitle("HELLO!")
+
+        QBtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        message = QLabel("Soll {} wirklich gelöscht werden?".format(item_name))
+        self.layout.addWidget(message)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec())
---
+++ flowlayout.py
@@ -0,0 +1,119 @@
+import typing
+
+from PyQt5.QtCore import Qt, QPoint, QRect, QSize
+from PyQt5.QtWidgets import QWidget, QLayout, QLayoutItem, QStyle, QSizePolicy
+
+
+class FlowLayout(QLayout):
+    def __init__(self, parent: QWidget=None, margin: int=-1, hSpacing: int=-1, vSpacing: int=-1):
+        super().__init__(parent)
+
+        self.itemList = list()
+        self.m_hSpace = hSpacing
+        self.m_vSpace = vSpacing
+
+        self.setAlignment(Qt.AlignTop)
+        self.setContentsMargins(margin, margin, margin, margin)
+
+    def __del__(self):
+        # copied for consistency, not sure this is needed or ever called
+        item = self.takeAt(0)
+        while item:
+            item = self.takeAt(0)
+
+    def addItem(self, item: QLayoutItem):
+        self.itemList.append(item)
+
+    def horizontalSpacing(self) -> int:
+        if self.m_hSpace >= 0:
+            return self.m_hSpace
+        else:
+            return self.smartSpacing(QStyle.PM_LayoutHorizontalSpacing)
+
+    def verticalSpacing(self) -> int:
+        if self.m_vSpace >= 0:
+            return self.m_vSpace
+        else:
+            return self.smartSpacing(QStyle.PM_LayoutVerticalSpacing)
+
+    def count(self) -> int:
+        return len(self.itemList)
+
+    def itemAt(self, index: int) -> typing.Union[QLayoutItem, None]:
+        if 0 <= index < len(self.itemList):
+            return self.itemList[index]
+        else:
+            return None
+
+    def takeAt(self, index: int) -> typing.Union[QLayoutItem, None]:
+        if 0 <= index < len(self.itemList):
+            return self.itemList.pop(index)
+        else:
+            return None
+
+    def expandingDirections(self) -> Qt.Orientations:
+        return Qt.Orientations(Qt.Orientation(0))
+
+    def hasHeightForWidth(self) -> bool:
+        return True
+
+    def heightForWidth(self, width: int) -> int:
+        height = self.doLayout(QRect(0, 0, width, 0), True)
+        return height
+
+    def setGeometry(self, rect: QRect) -> None:
+        super().setGeometry(rect)
+        self.doLayout(rect, False)
+
+    def sizeHint(self) -> QSize:
+        return self.minimumSize()
+
+    def minimumSize(self) -> QSize:
+        size = QSize()
+        for item in self.itemList:
+            size = size.expandedTo(item.minimumSize())
+
+        margins = self.contentsMargins()
+        size += QSize(margins.left() + margins.right(), margins.top() + margins.bottom())
+        return size
+
+    def smartSpacing(self, pm: QStyle.PixelMetric) -> int:
+        parent = self.parent()
+        if not parent:
+            return -1
+        elif parent.isWidgetType():
+            return parent.style().pixelMetric(pm, None, parent)
+        else:
+            return parent.spacing()
+
+    def doLayout(self, rect: QRect, testOnly: bool) -> int:
+        left, top, right, bottom = self.getContentsMargins()
+        effectiveRect = rect.adjusted(+left, +top, -right, -bottom)
+        x = effectiveRect.x()
+        y = effectiveRect.y()
+        lineHeight = 0
+
+        for item in self.itemList:
+            wid = item.widget()
+            spaceX = self.horizontalSpacing()
+            if spaceX == -1:
+                spaceX = wid.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Horizontal)
+            spaceY = self.verticalSpacing()
+            if spaceY == -1:
+                spaceY = wid.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Vertical)
+
+            nextX = x + item.sizeHint().width() + spaceX
+            if nextX - spaceX > effectiveRect.right() and lineHeight > 0:
+                x = effectiveRect.x()
+                y = y + lineHeight + spaceY
+                nextX = x + item.sizeHint().width() + spaceX
+                lineHeight = 0
+
+            if not testOnly:
+                item.setGeometry(QRect(QPoint(x, y), item.sizeHint()))
+    
+            x = nextX
+            lineHeight = max(lineHeight, item.sizeHint().height())
+
+        return y + lineHeight - rect.y() + bottom
+
